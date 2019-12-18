e51c6f4b27472e514fbf47308a0d7e00cb95c307
Brent Klein <bklein@xometry.com>
2019-12-18 14:14:54 -0500

    Added example of applying permissions to nested fields

--- app.py
+++ app.py
@@ -12,50 +12,6 @@ from strawberry.asgi import GraphQL as ASGIGraphQL
 from strawberry.field import strawberry_field
 
 
-# Define GraphQL types
-@strawberry.type
-class User:
-    name: str
-    age: int
-    nickname: Optional[str]
-
-    # @strawberry.field marks a method as a "resolver" in graphql parlance
-    # Specifying this as a resolver causes it to be calculated only when requested by the client
-    @strawberry.field
-    def rating(self, info: GraphQLResolveInfo) -> float:
-        return random.random()
-
-
-@strawberry.type
-class UserGroup:
-    name: str
-    users: List[User]
-
-    def __init__(self, name: str = "default") -> None:
-        self.name = name
-        self.users = []
-
-
-@strawberry.type
-class Company:
-    name: str
-    user_groups: List[UserGroup]
-
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.user_groups = []
-
-    def get_user_group(self, name: Optional[str]) -> Optional[UserGroup]:
-        group_names = [user_group.name for user_group in self.user_groups]
-        try:
-            if name is None:
-                name = ""
-            group_index = group_names.index(name)
-            return self.user_groups[group_index]
-        except ValueError:
-            return None
-
-
 # Define Permission classes
 class XometryPermission(BasePermission, ABC):
     """
@@ -96,6 +52,50 @@ class GandalfPermission(XometryPermission):
     def has_permission(self, info: GraphQLResolveInfo) -> bool:
         self.message = "Trying to pass? I don't think you shall."
         return False
+
+
+# Define GraphQL types
+@strawberry.type
+class User:
+    name: str
+    age: int
+    nickname: Optional[str]
+
+    # @strawberry.field marks a method as a "resolver" in graphql parlance
+    # Specifying this as a resolver causes it to be calculated only when requested by the client
+    @strawberry.field(permission_classes=[AdminPermission])
+    def rating(self, info: GraphQLResolveInfo) -> float:
+        return random.random()
+
+
+@strawberry.type
+class UserGroup:
+    name: str
+    users: List[User]
+
+    def __init__(self, name: str = "default") -> None:
+        self.name = name
+        self.users = []
+
+
+@strawberry.type
+class Company:
+    name: str
+    user_groups: List[UserGroup]
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.user_groups = []
+
+    def get_user_group(self, name: Optional[str]) -> Optional[UserGroup]:
+        group_names = [user_group.name for user_group in self.user_groups]
+        try:
+            if name is None:
+                name = ""
+            group_index = group_names.index(name)
+            return self.user_groups[group_index]
+        except ValueError:
+            return None
 
 
 # Define custom field decorators to collect reusable permissions
